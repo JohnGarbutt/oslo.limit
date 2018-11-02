0d6fa80ab5548af3ed5beda96ba381f0eddf8fdd
John Garbutt <john@johngarbutt.com>
2018-11-02 17:38:29 +0000

    Rework the enforcer with a new idea
    Change-Id: If6b149fbc5c1d0048fa4a834b532b76ba17d93aa

--- oslo_limit/limit.py
+++ oslo_limit/limit.py
@@ -16,7 +16,6 @@ from keystoneauth1 import loading as ksa_loading
 from openstack import connection
 from oslo_config import cfg
 from oslo_log import log
-import six
 
 from oslo_limit import exception
 from oslo_limit import opts
@@ -47,95 +46,34 @@ def _get_keystone_connection():
     return _SDK_CONNECTION
 
 
-class ProjectClaim(object):
-
-    def __init__(self, project_id, **kwargs):
-        """An object representing a claim of resources against a project.
-
-        :param project_id: The ID of the project claiming the resources.
-        :type project_id: string
-
-        """
-
-        if not isinstance(project_id, six.string_types):
-            msg = 'project_id must be a string type.'
-            raise ValueError(msg)
-
-        self.claims = {}
-        self.project_id = project_id
-
-        for resource_name, quantity in kwargs:
-            self.add_resource(resource_name, quantity)
-
-    def add_resource(self, resource_name, quantity):
-        """Add a resource type and quantity to a claim.
-
-        :param resource_name: A string representing the resource to claim.
-        :type resource_name: string
-        :param quantity: The number of resources being claimed.
-        :type quantity: integer
-
-        """
-
-        if not isinstance(resource_name, six.string_types):
-            msg = 'resource_name must be a string type.'
-            raise ValueError(msg)
-
-        if quantity and not isinstance(quantity, int):
-            msg = 'quantity must be an integer.'
-            raise ValueError(msg)
-
-        self.claims[resource_name] = quantity
-
-
 class Enforcer(object):
 
-    def __init__(self, claim, callback=None, verify=True):
-        """Context manager for checking usage against resource claims.
+    def __init__(self, callback=None):
+        """For checking usage against resource claims.
 
-        :param claim: An object containing information about the claim.
-        :type claim: ``oslo_limit.limit.ProjectClaim``
         :param callback: A callable function that accepts a project_id string
                          as a parameter and calculates the current usage of a
                          resource.
         :type callable function:
-        :param verify: Boolean denoting whether or not to verify the new usage
-                       after executing a claim. This can be useful for handling
-                       race conditions between clients claiming resources.
-        :type verify: boolean
-
         """
 
-        if not isinstance(claim, ProjectClaim):
-            msg = 'claim must be an instance of oslo_limit.limit.ProjectClaim.'
-            raise ValueError(msg)
         if callback and not callable(callback):
             msg = 'callback must be a callable function.'
             raise ValueError(msg)
-        if verify and not isinstance(verify, bool):
-            msg = 'verify must be a boolean value.'
-            raise ValueError(msg)
 
-        self.claim = claim
         self.callback = callback
-        self.verify = verify
         self._connection = _get_keystone_connection()
         self._service_id, self._region_id = self._get_service_and_region()
-        self._resource_limit = None
-
-    def __enter__(self):
-        if not self._resource_limit:
-            self._resource_limit = self._get_resource_limit()
-        self._verify(self._resource_limit)
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if not exc_val:
-            if self.verify:
-                self._verify(self._resource_limit)
 
     # FixMe(wxy): enable caching function. See bug 1790894
-    def _get_resource_limit(self):
+    def _get_service_and_region(self):
+        """Get service id and region id of the service."""
+        endpoint_id = CONF.oslo_limit.endpoint_id
+        endpoint = self._connection.get_endpoint(endpoint_id)
+        return endpoint.service_id, endpoint.region_id
+
+    # FixMe(wxy): enable caching function. See bug 1790894
+    def _get_resource_limit(self, project_id, resource_name):
         """Get the claimed resource's limit.
 
         Return registered limit instead if the project list does not exist.
@@ -143,8 +81,8 @@ class Enforcer(object):
         limit = self._connection.limits(
             service_id=self._service_id,
             region_id=self._region_id,
-            resource_name=self.claim.resource_name,
-            project_id=self.claim.project_id)
+            resource_name=resource_name,
+            project_id=project_id)
         try:
             project_limit = next(limit)
             return project_limit.resource_limit
@@ -152,25 +90,83 @@ class Enforcer(object):
             limit = self._connection.registered_limits(
                 service_id=self._service_id,
                 region_id=self._region_id,
-                resource_name=self.claim.resource_name)
+                resource_name=resource_name)
             try:
                 registered_limit = next(limit)
                 return registered_limit.default_limit
             except StopIteration:
                 raise exception.LimitNotFound(self.claim.resource_name)
 
-    # FixMe(wxy): enable caching function. See bug 1790894
-    def _get_service_and_region(self):
-        """Get service id and region id of the service."""
+    def _get_all_limits(self, project_id, resource_names):
+        limits = {}
+        for resource_name in resource_names:
+            limits[resource_name] = self._get_resource_limit(project_id,
+                                                             resource_names)
+        return limits
 
-        endpoint_id = CONF.oslo_limit.endpoint_id
-        endpoint = self._connection.get_endpoint(endpoint_id)
-        return endpoint.service_id, endpoint.region_id
+    def check_limits(self, context, project_id, extra_resources):
+        """Exception if currently over any of the specified limits.
 
-    def _verify(self, limit):
-        resource_names = self.claim.claims.keys()
-        usage = self.callback(self.claim.project_id, resource_names)
-        quantity = self.claim.quantity
-        if usage + quantity > limit:
-            raise exception.ClaimExceedsLimit(usage, quantity, limit,
-                                              self.claim.resource_name)
+        :param extra_resources: a dict that contains the resources
+            that need to be checked and how many extra resources are
+            required e.g.:
+               {'instances': 1, 'resources:VCPU': 2, 'resources:MEMORY_MB': 3}
+            Or if you just want to check if already over limit:
+               {'instances': 0, 'resources:VCPU': 0, 'resources:MEMORY_MB': 0}
+        """
+        # TODO(johngarbutt) clearly need to be more defensive here!
+        resource_names = extra_resources.keys()
+
+        limits = self._get_all_limits(project_id, resource_names)
+        usages = self.callback(context, project_id, resource_names)
+
+        for resource_name in resource_names:
+            limit = int(limits[resource_name])
+            usage = int(usages[resource_name])
+            extra = int(extra_resources.get(resource_name, 0))
+
+            # TODO(johngarbutt) clearly should return all problems,
+            # not just the first one! Also note extra may be zero here.
+            if usage + extra > limit:
+                raise exception.ClaimExceedsLimit(usage, extra, limit,
+                                                  resource_name)
+
+        # TODO(johngarbutt) Seems odd not to return this, but I don't need it
+        return limits, usages
+
+    def verify_claim(self, project_id, extra_resources):
+        """Get a context manager to double check claim."""
+        return EnforcerContext(self, project_id, extra_resources)
+
+
+# NOTE: not sure we need this in the first version...
+class EnforcerContext(object):
+
+    def __init__(self, enforcer, project_id, extra_resources):
+        """Context manager for checking usage against resource claims.
+
+        It will verify the new usage after executing a claim. This can be
+        useful for handling race conditions between clients claiming resources.
+        It assumes the resource usage counting will now include the
+        extra_resources that were originally requested.
+        """
+        self._enforcer = enforcer
+        self._project_id = project_id
+        self._extra_resources = extra_resources
+        self.limits = None
+        self.usages = None
+
+    def __enter__(self):
+        # allow the user to check out what is being used and limited
+        # although probably more useful in the error case, annoyingly
+        self.limits, self.usages = self.enforcer.check_limits(
+            self._project_id, self._extra_resources)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not exc_val:
+            resource_names = self._extra_resources.keys()
+            extra_resources = {name: 0 for name in resource_names}
+            self.enforcer.check_limits(self._project_id, extra_resources)
+            # TODO(johngarbutt) should we have some rollback function that gets
+            # called when we hit and error here?
