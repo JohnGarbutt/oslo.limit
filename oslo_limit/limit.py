6c7628b5a555f530430490e8c8b37f84c20d2253
John Garbutt <john@johngarbutt.com>
2019-11-20 22:22:48 +0000

    Add basic enforcer
    Taking the Nova work as an example, looking to add a basic flat enforcer
    that meets Nova's needs.
    
    This builds on various previous prototypes from:
    Co-Authored-By: Lance Bragstad<lbragstad@gmail.com>
    Co-Authored-By: wangxiyuan <wangxiyuan@huawei.com>
    
    Change-Id: I294a922ea80af673291c991f07a4a203f25c289d

--- oslo_limit/limit.py
+++ oslo_limit/limit.py
@@ -12,10 +12,24 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from keystoneauth1 import exceptions as ksa_exception
+from keystoneauth1 import loading as ksa_loading
+from openstack import connection
+from oslo_config import cfg
+from oslo_log import log
 import six
 
+from oslo_limit import exception
+from oslo_limit import opts
 
-class Enforcer(object):
+LOG = log.getLogger(__name__)
+CONF = cfg.CONF
+_SDK_CONNECTION = None
+
+opts.register_opts(CONF)
+
+
+class _FlatEnforcer(object):
 
     def __init__(self, usage_callback):
         """An object for checking usage against resource limits and requests.
@@ -24,38 +38,144 @@ class Enforcer(object):
                                string as a parameter and calculates the current
                                usage of a resource.
         :type callable function:
-
         """
         if not callable(usage_callback):
             msg = 'usage_callback must be a callable function.'
             raise ValueError(msg)
-
         self.usage_callback = usage_callback
 
-    def enforce(self, project_id, deltas, resource_filters=None):
-        """Check resource usage against limits and request deltas.
+        keystone_connection = _get_keystone_connection()
+        self.utils = _EnforcerUtils(keystone_connection)
+
+    def enforce(self, project_id, deltas=None):
+        """Check resource usage against limits
+
+        If deltas are specified, add deltas to exiting usage.
+        Note we fail for any resource usage over the limit, not just
+        the ones specified in the deltas.
 
         :param project_id: The project to check usage and enforce limits
                            against.
         :type project_id: string
         :param deltas: An dictionary containing resource names as keys and
                        requests resource quantities as values.
-        :type deltas: dictionary
-        :param resource_filters: A list of strings containing the resource
-                                 names to filter the return values of the
-                                 usage_callback. This is a performance
-                                 optimization in the event the caller doesn't
-                                 want the usage_callback to collect all
-                                 resources owned by the service. By default,
-                                 no resources will be filtered.
-
+        :type deltas: dictionary or None
         """
-        if not isinstance(project_id, six.text_type):
-            msg = 'project_id must be a string.'
+        if not project_id or not isinstance(project_id, six.string_types):
+            msg = 'project_id must be a non-empty string.'
             raise ValueError(msg)
-        if not isinstance(deltas, dict):
+        if deltas is not None and not isinstance(deltas, dict):
             msg = 'deltas must be a dictionary.'
             raise ValueError(msg)
-        if not isinstance(resource_filters, list):
-            msg = 'resource_filters must be a list.'
-            raise ValueError(msg)
+
+        resource_names = self.utils.get_all_registered_limit_resources()
+        # sort names for predictable failure order
+        resource_names = sorted(list(resource_names))
+
+        if deltas is None:
+            deltas = {}
+
+        for resource in deltas.keys():
+            if resource not in resource_names:
+                msg = "unexpected resource %s in deltas" % resource
+                raise ValueError(msg)
+
+        all_deltas = {}
+        for resource_name in resource_names:
+            all_deltas[resource_name] = deltas.get(resource_name, 0)
+
+        counts = self.usage_callback(project_id, resource_names)
+
+        overs = []
+        for resource_name in resource_names:
+            delta = all_deltas[resource_name]
+            if resource_name not in counts:
+                msg = "missing resource counts for %s" % resource_name
+                raise ValueError(msg)
+            count = counts.get(resource_name, 0)
+            limit = self.utils.get_limit(project_id, resource_name)
+
+            if int(count) + int(delta) > int(limit):
+                overs.append((count, delta, limit, resource_name))
+
+        if len(overs) > 0:
+            LOG.debug("hit limits for project %s %s", project_id, overs)
+            usage, delta, limit, resource_name = overs[0]
+            raise exception.ClaimExceedsLimit(usage, delta, limit,
+                                              resource_name)
+
+
+# TODO(johngarbutt) we should load this based on keystone settings
+Enforcer = _FlatEnforcer
+
+
+def _get_keystone_connection():
+    global _SDK_CONNECTION
+
+    if not _SDK_CONNECTION:
+        try:
+            auth = ksa_loading.load_auth_from_conf_options(
+                CONF, group='oslo_limit')
+            session = ksa_loading.load_session_from_conf_options(
+                CONF, group='oslo_limit', auth=auth)
+            _SDK_CONNECTION = connection.Connection(session=session).identity
+        except (ksa_exception.NoMatchingPlugin,
+                ksa_exception.MissingRequiredOptions,
+                ksa_exception.MissingAuthPlugin,
+                ksa_exception.DiscoveryFailure,
+                ksa_exception.BadRequest,
+                ksa_exception.Unauthorized) as e:
+            msg = "Can't initialise OpenStackSDK session, reason: %s" % e
+            LOG.error(msg)
+            raise exception.SessionInitError(e)
+
+    return _SDK_CONNECTION
+
+
+class _EnforcerUtils(object):
+    def __init__(self, keystone_connection):
+        self.connection = keystone_connection
+
+        # cache endpoint info
+        endpoint_id = CONF.oslo_limit.endpoint_id
+        self.endpoint = self.connection.get_endpoint(endpoint_id)
+        if not self.endpoint:
+            raise ValueError("can't find endpoint for %s" % endpoint_id)
+
+    def get_all_registered_limit_resources(self):
+        reg_limits = self.connection.registered_limits(
+            service_id=self.endpoint.service_id,
+            region_id=self.endpoint.region_id)
+        return [limit.resource_name for limit in reg_limits]
+
+    def get_limit(self, project_id, resource_name):
+        # TODO(johngarbutt): might need to cache here
+        project_limit = self._get_project_limit(project_id, resource_name)
+        if project_limit:
+            return project_limit.resource_limit
+        registered_limit = self._get_registered_limit(resource_name)
+        if registered_limit:
+            return registered_limit.default_limit
+
+        raise exception.LimitNotFound(resource_name)
+
+    def _get_project_limit(self, project_id, resource_name):
+        limit = self.connection.limits(
+            service_id=self.endpoint.service_id,
+            region_id=self.endpoint.region_id,
+            resource_name=resource_name,
+            project_id=project_id)
+        try:
+            return next(limit)
+        except StopIteration:
+            return None
+
+    def _get_registered_limit(self, resource_name):
+        reg_limit = self.connection.registered_limits(
+            service_id=self.endpoint.service_id,
+            region_id=self.endpoint.region_id,
+            resource_name=resource_name)
+        try:
+            return next(reg_limit)
+        except StopIteration:
+            return None
--- oslo_limit/tests/test_limit.py
+++ oslo_limit/tests/test_limit.py
@@ -15,25 +15,27 @@
 """
 test_limit
 ----------------------------------
-
 Tests for `limit` module.
 """
 
+import mock
 import uuid
 
+from openstack.identity.v3 import registered_limit
 from oslotest import base
 
+from oslo_limit import exception
 from oslo_limit import limit
 
 
+@mock.patch.object(limit, "_get_keystone_connection", new=mock.MagicMock)
 class TestEnforcer(base.BaseTestCase):
 
     def setUp(self):
         super(TestEnforcer, self).setUp()
-        self.deltas = dict()
 
-    def _get_usage_for_project(self, project_id):
-        return 8
+    def _get_usage_for_project(self, project_id, resource_names):
+        return {name: 1 for name in resource_names}
 
     def test_required_parameters(self):
         enforcer = limit.Enforcer(self._get_usage_for_project)
@@ -61,3 +63,79 @@ class TestEnforcer(base.BaseTestCase):
                 project_id,
                 invalid_delta
             )
+
+    def test_project_id_must_be_a_string(self):
+        enforcer = limit.Enforcer(self._get_usage_for_project)
+        invalid_delta_types = [{}, 5, 5.1, True, False, [], None, ""]
+        for invalid_project_id in invalid_delta_types:
+            self.assertRaises(
+                ValueError,
+                enforcer.enforce,
+                invalid_project_id)
+
+    @mock.patch.object(limit._EnforcerUtils,
+                       "get_limit")
+    @mock.patch.object(limit._EnforcerUtils,
+                       "get_all_registered_limit_resources")
+    def test_enforce(self, mock_get_all, mock_limit):
+        def fake_callback(proj_id, resources):
+            return {
+                "a": 5,
+                "b": 10,
+                "c": 0,
+            }
+
+        enforcer = limit.Enforcer(fake_callback)
+
+        project_id = uuid.uuid4().hex
+        enforcer.enforce(project_id)
+
+        e = self.assertRaises(ValueError,
+                              enforcer.enforce,
+                              project_id, {"a": 8})
+        self.assertEqual(str(e), "unexpected resource a in deltas")
+
+        mock_get_all.return_value = ["a", "b", "c"]
+        mock_limit.return_value = 10
+        enforcer.enforce(project_id, {"a": 5})
+
+        e = self.assertRaises(exception.ClaimExceedsLimit,
+                              enforcer.enforce,
+                              project_id, {"a": 6})
+        self.assertEqual(str(e),
+                         "5 a have been used. Claiming 6 a would exceed "
+                         "the current limit of 10")
+
+        mock_get_all.return_value = ["a", "b", "c", "d"]
+        e = self.assertRaises(ValueError,
+                              enforcer.enforce,
+                              project_id, {"a": 5})
+        self.assertEqual(str(e),
+                         "missing resource counts for d")
+
+
+class TestEnforcerUtils(base.BaseTestCase):
+    def test_get_endpoint(self):
+        mock_conn = mock.MagicMock()
+        mock_conn.get_endpoint.return_value = "fake"
+
+        utils = limit._EnforcerUtils(mock_conn)
+
+        self.assertEqual("fake", utils.endpoint)
+        mock_conn.get_endpoint.assert_called_once_with(None)
+
+    def test_get_all_registered_limit_resources(self):
+        mock_conn = mock.MagicMock()
+        utils = limit._EnforcerUtils(mock_conn)
+
+        mock_conn.registered_limits.return_value = []
+
+        names = utils.get_all_registered_limit_resources()
+        self.assertEqual([], names)
+
+        foo = registered_limit.RegisteredLimit()
+        foo.resource_name = "foo"
+        mock_conn.registered_limits.return_value = [foo]
+
+        names = utils.get_all_registered_limit_resources()
+        self.assertEqual(["foo"], names)
